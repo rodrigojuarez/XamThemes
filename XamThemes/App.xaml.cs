6957700917ecc1adfdc23e33ad01f687afd3ca97
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2021-02-08 00:59:56 -0300

    Initial Commit

---
+++ XamThemes/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamThemes
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         var theme = Preferences.Get("OSAppTheme", Enum.GetName(typeof(OSAppTheme), OSAppTheme.Unspecified));
+         App.Current.UserAppTheme = (OSAppTheme)Enum.Parse(typeof(OSAppTheme), theme);
+
+         MainPage = new MainPage();
+      }
+   }
+}
---
+++ XamThemes/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.ObjectModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamThemes.ViewModel
+{
+   public class MainViewModel : BaseViewModel
+   {
+      public MainViewModel()
+      {
+         var theme = Preferences.Get("OSAppTheme", Enum.GetName(typeof(OSAppTheme), Xamarin.Forms.OSAppTheme.Unspecified));
+         OSAppTheme = (int)Enum.Parse(typeof(OSAppTheme), theme);
+      }
+
+      public ObservableCollection<string> Themes => new ObservableCollection<string> { "System Default", "Light", "Dark" };
+
+      int osAppTheme;
+      public int OSAppTheme
+      {
+         get => osAppTheme;
+         set
+         {
+            if (osAppTheme == value)
+            {
+               return;
+            }
+
+            Preferences.Set("OSAppTheme", Enum.GetName(typeof(OSAppTheme), value));
+            App.Current.UserAppTheme = (OSAppTheme)value;
+            SetProperty(ref osAppTheme, value);
+         }
+
+      }
+   }
+}
